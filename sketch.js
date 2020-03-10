7effe457064f4867ed8ec9129fb50bffaac83a4e
Bilot Tristan <tristan_bilot@carrefour.com>
2020-03-10 15:53:29 +0100

    refactoring

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 edges = [];
-vertices = {}; // ex: 2 => Vertex(2)
-groups = []; // 0 => [(0,1), (0,2)...]
+vertices = {};  /*  2  =>  Vertex(2) */
+groups = [];    /* [0] => [(0,1), (0,2)...] */
 
 function setup() {
     createCanvas(800, 500, WEBGL);
@@ -23,12 +23,13 @@ function draw() {
 }
 
 function build_groups() {
-    groupIndex = -1;
+    groupIndex = 0;
     edges.forEach((pair) => {
         found = false;
         for (let i = 0; i < groups.length; i++) {
             for (let j = 0; j < groups[i].length; j++) {
-                let currentPair = groups[i][j]; // each vertex is compared to the pair
+                /* each vertex is compared to the pair */
+                let currentPair = groups[i][j];
                 if (twoPairsMatch(currentPair, pair)) {
                     groups[i].push(pair);
                     found = true;
@@ -37,18 +38,10 @@ function build_groups() {
             }
         }
         if (!found) {
-            groups[++groupIndex] = [];
-            groups[groupIndex].push(pair);
+            groups[groupIndex] = [];
+            groups[groupIndex++].push(pair);
         }
     });
-}
-
-function twoPairsMatch(vertex, pair) {
-    return (pair[0] == vertex[0] || 
-            pair[0] == vertex[1] || 
-            pair[1] == vertex[0] || 
-            pair[1] == vertex[1]
-        )
 }
 
 function build_vertices() {
@@ -61,9 +54,9 @@ function build_vertices() {
     var top = true;
 
     visited = {};
-    for (let i = 0; i < nbGroups; i++) {                // group i
-        for (let j = 0; j < groups[i].length; j++) {    // element of group i
-            for (let k = 0; k < 2; k++) {               // (x,y) tuple of element
+    for (let i = 0; i < nbGroups; i++) {                /* group i */
+        for (let j = 0; j < groups[i].length; j++) {    /* element of group i */
+            for (let k = 0; k < 2; k++) {               /* (x,y) tuple of element */
                 v = groups[i][j][k];
                 if (visited[v] == null) {
                     visited[v] = true;
@@ -77,21 +70,6 @@ function build_vertices() {
         }
     }
 }
-
-// function build_vertices() {
-//     visited = {};
-//     edges.forEach((pair) => {
-//         for (let i = 0; i < pair.length; i++) {
-//             v = pair[i];
-//             if (visited[v] == null) {
-//                 visited[v] = true;
-//                 x = (floor(random(-width/3, width/3)) + width/150);
-//                 y = (floor(random(-width/3, width/3)) + height/150);
-//                 vertices[v] = new Vertex(x, y, v);
-//             }
-//         }
-//     });
-// }
 
 function draw_edges() {
     beginShape(LINES);
@@ -113,4 +91,12 @@ function draw_vertices() {
         object = vertices[v];
         ellipse(object.x, object.y, 50, 50);
     }
+}
+
+function twoPairsMatch(vertex, pair) {
+    return (pair[0] == vertex[0] || 
+            pair[0] == vertex[1] || 
+            pair[1] == vertex[0] || 
+            pair[1] == vertex[1]
+        )
 }
